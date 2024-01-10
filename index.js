a00ae105b213ff885e57289b0ce7c925cd1d1075
Abhinav Panwar <95019283+ABHINAVpanwar@users.noreply.github.com>
2024-01-10 21:36:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.addEventListener("load", function () {
   this.document.getElementById("PL").style.display = "none";
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var textElement = document.getElementById("T1");
   textElement.classList.add("animation");
   textElement.style.opacity = 1; // Set opacity to 1 to make the text visible
@@ -288,6 +288,8 @@ function updateExperience() {
       experienceElement.textContent = targetExperience; // Ensure the final value is exact
       clearInterval(update);
       plus.style.display = "block";
+      experienceElement.style.color = "#ff6c68";
+      plus.style.color = "#ff6c68";
     }
   }, updateInterval);
 }
